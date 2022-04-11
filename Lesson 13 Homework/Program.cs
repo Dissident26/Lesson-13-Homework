47fc79b4b084d3ee82db912472986eb42ee62f1f
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-04-11 10:00:04 +0300

    Add project files.

---
+++ Lesson 13 Homework/Program.cs
@@ -0,0 +1,29 @@
+﻿using ProductClass;
+
+var products = new ProductList();
+products.AddProducts(new[]
+{
+    new Product("product 1", "shop #1"),
+    new Product("product 1", "shop #2"),
+    new Product("product 2", "shop #1"),
+    new Product("product 2", "shop #2"),
+    new Product("product 1", "shop #3"),
+});
+
+products.SortByRetailerName();
+GetProductListByShopName();
+
+void GetProductListByShopName()
+{
+    string shopName = Console.ReadLine();
+
+    if(shopName != null)
+    {
+        var filteredList = products?.FilterByRetailerName(shopName);
+
+        foreach (var item in filteredList)
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+}
---
+++ ProductClass/Product.cs
@@ -0,0 +1,51 @@
+﻿namespace ProductClass
+{
+    public class Product: IComparable
+    {
+        public string Name;
+        public string RetailerName;
+        public Product(string name, string retailerName)
+        {
+            Name = name;
+            RetailerName = retailerName;
+         }
+
+        public int CompareTo(Product? obj)
+        {
+            return RetailerName.CompareTo(obj?.RetailerName);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            return Name.CompareTo(obj);
+        }
+    }
+
+    public class ProductList
+    {
+        public List<Product> Products = new();
+
+        public void SortByRetailerName()
+        {
+            Products?.Sort((x, y) => x.CompareTo(y));
+        }
+        public List<Product>? FilterByRetailerName(string retailerName)
+        {
+            var result = Products?.FindAll(x => x.RetailerName == retailerName);
+            if(result?.Count == 0)
+            {
+                throw new ArgumentException($"Shop '{retailerName}' do not exist in list!");
+            }
+
+            return result;
+        }
+
+        public void AddProducts(Product[] products)
+        {
+            foreach (var item in products)
+            {
+                Products.Add(item);
+            }
+        }
+    }
+}
